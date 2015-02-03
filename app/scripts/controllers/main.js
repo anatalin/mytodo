8408957ece6d1e703bca8dde456b7d921e16dc93
Andrew Natalin <anatalin@outlook.com>
2015-02-03 18:24:21 +0300

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+	 var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore || [];
+	
+	$scope.$watch('todos', function() {
+		localStorageService.set('todos', $scope.todos);
+	}, true);
+	
+	$scope.addTodo = function() {
+		var isOkay = true;
+		$scope.todos.forEach(function(todo) {
+			if (todo === $scope.todo) {
+				isOkay = false;
+				return;
+			}
+		});
+		
+		if (!isOkay) {
+			return;
+		}
+		
+		$scope.todos.push($scope.todo);
+		$scope.todo = '';
+	};
+	
+	$scope.removeTodo = function(index) {
+		$scope.todos.splice(index, 1);
+	};
+  });
